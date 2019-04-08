8476ec945bf554e4d7087b4c9065936f2a161e95
qingcheng12 <1160752275@qq.com>
2019-04-08 09:57:32 +0800

    first commit

---
+++ sqrt-cmake/build/CMakeFiles/sqrt-cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sqrt-cmake.dir/src/b.c.o"
+  "CMakeFiles/sqrt-cmake.dir/src/main.c.o"
+  "sqrt-cmake.pdb"
+  "sqrt-cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sqrt-cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
